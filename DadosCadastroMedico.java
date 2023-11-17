dac2af8c34ac1f53802e51e7247e79a04d44ff62
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2023-11-17 19:13:23 -0300

    Delete DadosCadastroMedico.java

--- DadosCadastroMedico.java
+++
@@ -1,6 +0,0 @@
-package med.igr.api.medico;
-
-import med.igr.api.endereco.DadosEndereco;
-
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco ) {
-}
